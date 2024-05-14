445b3db843a84c49818a511f2ceae6c436aecff9
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-14 04:38:00 +0200

    install and configure postgresql

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("server is running here");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT ?? 8080;
+const app = express();
+
+app.get("/todos", (request, response) => {
+  response.send("<H1>Hello World ! </h1>");
+});
+app.listen(PORT, () => {
+  console.log("server is running on port: ", PORT);
+});
