6990d6baee4701b0146881745645b90da43ddccb
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-14 04:54:23 +0200

    connecting to database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { pool } = require("./database");
 dotenv.config();
 const PORT = process.env.PORT ?? 8080;
 const app = express();
 
-app.get("/todos", (request, response) => {
-  response.send("<H1>Hello World ! </h1>");
+app.get("/todos", async (request, response) => {
+  const email = "erick@yahoo.com";
+  try {
+    const getAlldata = await pool.query(
+      "SELECT * FROM todos WHERE user_email = $1",
+      [email]
+    );
+    response.status(200).json({
+      data: getAlldata.rows,
+    });
+    console.log(getAlldata.rows[0]);
+  } catch (error) {
+    console.log(`Error : ${error}`);
+  }
 });
+
 app.listen(PORT, () => {
   console.log("server is running on port: ", PORT);
 });
