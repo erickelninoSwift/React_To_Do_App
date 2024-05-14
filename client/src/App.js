c8f8d2b5f04de2a8ebdcf0c8d5cf7a23f088bc62
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-14 03:05:38 +0200

    added some packages from our app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+import ListHeader from "./components/ListHeader";
+
 const App = () => {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="app">
+      <ListHeader listName={"Holiday Tick list"} />
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const ListHeader = () => {
-  return <div>ListHeader</div>;
+const ListHeader = ({ listName }) => {
+  const signOut = () => {
+    console.log("signout");
+  };
+
+  const addNew = () => {
+    console.log("signout");
+  };
+  return (
+    <div className="list-header">
+      <h1> {listName}</h1>
+      <div className="button-container">
+        <button className="create" onClick={() => addNew()}>
+          ADD NEW
+        </button>
+        <button className="signout" onClick={() => signOut()}>
+          SIGN OUT
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ListHeader;
